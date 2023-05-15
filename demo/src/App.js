a8ad2da67da37959648c76e3c8b7042f3e743279
thong312 <lyminhthong312@gmail.com>
2023-05-15 16:40:40 +0700

    ex

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,14 +1,13 @@
 
 import './App.css';
 import Footer from './components/Footer';
+import Main from './components/Main';
 import Navigation from './components/Navigation';
-import Player from './components/Player';
-
 function App() {
   return (
     <div className="App">
       <Navigation/>
-      <Player/>
+      <Main/>
       <Footer/>
     </div>
   );
---
+++ demo/src/components/Players.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Players } from '../shared/ListOfPlayers'
+export default function demoPlayers() {
+    return (
+        <div className='container'>
+            {Players.map((player) => (
+                <div className='column'>
+                    <div className='card'>
+                        <img src={player.img} alt=''/>
+                        <h3>{player.name}</h3>
+                        <p className='title'>{player.club}</p>
+                        <p><button>Detail</button></p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
